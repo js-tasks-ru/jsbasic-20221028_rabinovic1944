074e4fd5b85dce5e7da9877a1edf50396dacbbc2
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-07 19:42:56 -0500

    hw_011_factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let resultFactorial;
+  if ((n == 0)||(n == 1)) {
+    return 1;
+  }
+  else
+    resultFactorial = n;
+  while (n > 1) {
+    resultFactorial *= (--n);
+  }
+  return resultFactorial;
 }
+
