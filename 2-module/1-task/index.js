eb15a34c1a1cadd92dcf8d7dd3899bced4a1b35f
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-09 23:00:50 -0500

    hw_21_Salaries_sum

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
+
+
 function sumSalary(salaries) {
   // ваш код...
+  let salariesSum = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])) {
+      salariesSum += salaries[key];
+    }
+  }
+return salariesSum;
 }
