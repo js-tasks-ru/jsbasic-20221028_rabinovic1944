e9e233466dd844c06fff6542bdd8662b0559a922
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-10 17:24:43 -0500

    hw_23 It didnt pass NPM but I have no idea why, it works fine exactly as needed

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,23 @@
 let calculator = {
-  // ваш код
-};
+  num1 : 0,
+  num2 : 0,
+  read(num1,num2) {
+    this.num1 = Number(prompt('Please enter first number', 0));
+    this.num2 = Number(prompt('Pleate enter second number', 0));
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
+
+}
+
+
+
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
