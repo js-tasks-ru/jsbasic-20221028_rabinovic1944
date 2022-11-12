30f9613714312330757312339b8a8a0b9c6dd304
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-11 23:06:15 -0500

    hw_23 calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -2,8 +2,8 @@ let calculator = {
   num1 : 0,
   num2 : 0,
   read(num1,num2) {
-    this.num1 = Number(prompt('Please enter first number', 0));
-    this.num2 = Number(prompt('Pleate enter second number', 0));
+    this.num1 = num1;
+    this.num2 = num2;
   },
   sum() {
     return this.num1 + this.num2;
@@ -13,7 +13,6 @@ let calculator = {
   },
 
 }
-
 
 
 
