a7b216f7cae7450ad174bdb505a21c9823b7232b
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-15 02:16:34 -0500

    hw_31_массив имен

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,26 @@
+////let vasya = { name: 'Вася', age: 25 };
+////let petya = { name: 'Петя', age: 30 };
+////let masha = { name: 'Маша', age: 28 };
+
+////let users = [vasya, petya, masha];
+
+////alert('lets start');
+
+
+
+//for (let key in petya)
+//  {
+//    alert(petya[key]);
+//  }
+
+
 function namify(users) {
-  // ваш код...
+
+  let usersNames = users.map(item => item.name);
+  return usersNames;
+  
 }
+
+
+
+//namify(users);
