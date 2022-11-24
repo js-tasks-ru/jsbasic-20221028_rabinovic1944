f70e21919935337f16ba3525c9e9036effd5ce67
rabinovic1944 <rabinovic1944@yahoo.com>
2022-11-24 02:43:00 -0500

    hw_43 function highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,48 @@
 function highlight(table) {
-  // ваш код...
+
+
+  let tableBodyRows = table.children[1].children;
+  let counter = table.rows.length - 2;
+  
+  while (counter > -1) {
+
+    if (tableBodyRows[counter].children[3].dataset.available == 'true') {
+
+      tableBodyRows[counter].classList.add('available');
+
+    }
+
+    else if (tableBodyRows[counter].children[3].dataset.available == undefined) {
+
+      tableBodyRows[counter].hidden = true;
+
+    }
+
+    else {
+
+      tableBodyRows[counter].classList.add('unavailable')
+
+    }
+
+    if (tableBodyRows[counter].children[2].innerText == 'm') {
+
+      tableBodyRows[counter].classList.add('male');
+
+    }
+
+    else {
+
+      tableBodyRows[counter].classList.add('female')
+
+    }
+
+    if (Number(tableBodyRows[counter].children[1].innerText) < 18) {
+
+      tableBodyRows[counter].style = "text-decoration: line-through";
+
+    }
+
+    counter--;
+  }
+
 }
