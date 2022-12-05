d19b300159872604d58f36266965c4dcbfceb6da
rabinovic1944 <rabinovic1944@yahoo.com>
2022-12-05 16:41:04 -0500

    hw_62 product card

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,41 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+  
   constructor(product) {
+
+  this.product = product;
+
+  const itemCard = createElement(`
+
+<div class="card">
+    <div class="card__top">
+        <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+        <span class="card__price">€${product.price}.00</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${product.name}</div>
+        <button type="button" class="card__button" >
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+</div>
+
+`);
+
+itemCard.elem = itemCard;
+
+let addButton = itemCard.elem.querySelector(".card__button");
+let plusEvent = new CustomEvent("product-add", {detail: this.product.id, bubbles: true });
+
+addButton.addEventListener('click', function () {
+  addButton.dispatchEvent(plusEvent);
+ });
+
+return itemCard.elem;
+
   }
+
 }
+
+ 
